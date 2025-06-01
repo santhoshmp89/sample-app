6ab2673afb78bd7a4dc28b6d582a9db7b1bb04c7
Santhosh Kumar <santhoshmp89@gmail.com>
2025-06-01 17:02:27 +0530

    Update common.js

--- common.js
+++ common.js
@@ -1,11 +1,12 @@
 window.addEventListener("pagehide", function () {
   alert("[pagehide] URL:", window.location.href);
-  console.log("[pageshow] URL:", window.location.href);
+  // console.log("[pageshow] URL:", window.location.href);
+  window.location = "http://www.google.com";
 });
 
-window.addEventListener("pageshow", function () {
-  console.log("[pageshow] URL:", window.location.href);
-});
+// window.addEventListener("pageshow", function () {
+//   console.log("[pageshow] URL:", window.location.href);
+// });
 
 // window.addEventListener("beforeunload", function () {
 //   console.log("[beforeunload] URL:", window.location.href);
