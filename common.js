511384feacd0bbeb49a1510be67e923379df4fab
Santhosh Kumar <santhoshmp89@gmail.com>
2025-06-01 16:06:18 +0530

    Update common.js

--- common.js
+++ common.js
@@ -2,6 +2,10 @@ window.addEventListener("pagehide", function () {
   console.log("[pagehide] URL:", window.location.href);
 });
 
+window.addEventListener("pageshow", function () {
+  console.log("[pageshow] URL:", window.location.href);
+});
+
 // window.addEventListener("beforeunload", function () {
 //   console.log("[beforeunload] URL:", window.location.href);
 // });
