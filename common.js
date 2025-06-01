41642672765b7cb0b75426cb02992866b3f5d9f5
AzureAD\SanthoshKumarM <mskumat@catchpoint.com>
2025-06-01 15:52:34 +0530

    dummy

---
+++ common.js
@@ -0,0 +1,11 @@
+window.addEventListener("pagehide", function () {
+  console.log("[pagehide] URL:", window.location.href);
+});
+
+// window.addEventListener("beforeunload", function () {
+//   console.log("[beforeunload] URL:", window.location.href);
+// });
+
+// document.addEventListener("visibilitychange", () => {
+//   console.log(`[visibilitychange] State: ${document.visibilityState}`);
+// });
